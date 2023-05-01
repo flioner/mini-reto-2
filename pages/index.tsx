2ac65344afbd364396304654861d22e47cacaa8b
flioner <lionerrochae@gmail.com>
2023-04-30 21:10:10 -0600

    main styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,26 +21,28 @@ export default function Home() {
   return (
     <Layout>
       <div className={s.mainCont}>
-        <div className={s.tareasCont}>
-          <h2>Pendientes</h2>
-          <ul>
-            {tareasPendientes.map((tarea) => (
-              <li key={tarea.id}>{tarea.descripcion}</li>
-            ))}
-          </ul>
-        </div>
-
-        <div className={s.together}>
-          <div className={s.compCont}>
-            <h2>Completas</h2>
+        <div className={s.contCont}>
+          <div className={s.tareasCont}>
+            <div className={s.title}>Pendientes</div>
             <ul>
-              {tareasCompletas.map((tarea) => (
+              {tareasPendientes.map((tarea) => (
                 <li key={tarea.id}>{tarea.descripcion}</li>
               ))}
             </ul>
           </div>
-          <div className={s.nuevaCont}>
-            <button className={s.nueva}> Nueva Tarea</button>
+
+          <div className={s.together}>
+            <div className={s.compCont}>
+              <div className={s.title}>Completas</div>
+              <ul>
+                {tareasCompletas.map((tarea) => (
+                  <li key={tarea.id}>{tarea.descripcion}</li>
+                ))}
+              </ul>
+            </div>
+            <div className={s.nuevaCont}>
+              <button className={s.nueva}> Nueva Tarea</button>
+            </div>
           </div>
         </div>
       </div>
